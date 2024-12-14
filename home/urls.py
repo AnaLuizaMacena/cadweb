5f31319c7cd1415b6353d06ba66c3209bc8c3200
Ana Luiza Macena Pereira <159857048+AnaLuizaMacena@users.noreply.github.com>
2024-12-14 14:27:35 -0300

    crud

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('categoria/', views.categoria, name="categoria" ),
     path('categoria/form', views.form_categoria, name="form_categoria" ),
+    path('editar_categoria/<int:id>/', views.editar_categoria, name="editar_categoria" ),
+    path('detalhes_categoria/<int:id>/', views.detalhes_categoria, name="detalhes_categoria" ),
+    path('excluir_categoria/<int:id>/', views.excluir_categoria, name="excluir_categoria" ),
 ]
